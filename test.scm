b99c4e4023171c4d2ce7cc420c3cb82206138954
kai <kai@deathmetal>
2011-09-28 20:39:42 +0200

    shader convenience function, using render-data which might just be my new convention...

--- test.scm
+++ test.scm
@@ -1,33 +1,35 @@
 (display "oh yeah!")(newline)
 
-(define vsrc "
-		#version 150 core
-		in vec3 in_pos;
-		uniform mat4 proj;
-		uniform mat4 moview;
-		void main() {
-			gl_Position = proj * moview * vec4(in_pos,1);
-		}
-		")
-(define fsrc "
-		#version 150 core
-		out vec4 out_col;
-		uniform vec3 line_col;
-		void main() {
-			out_col = vec4(line_col,1);
-		}
-		")
-(display "----->")(display
-(let ((shader (make-shader "line-shader" 1)))
- (display shader)(display "  ")
-  (add-vertex-source shader vsrc)
-  (add-fragment-source shader fsrc)
-  (add-shader-input shader "in_pos" 0)
-  (compile-and-link-shader shader))
-	)(newline)
+(define make-shader-ll make-shader)
+(define (make-shader name vsrc fsrc inputs)
+  (let ((shader (make-shader-ll name (length inputs))))
+    (add-vertex-source shader vsrc)
+    (add-fragment-source shader fsrc)
+	(let rec ((rest inputs) (i 0))
+      (add-shader-input shader (car rest) i)
+      (if (not (null? (cdr rest)))
+          (rec (cdr rest) (1+ i))))
+    (compile-and-link-shader shader)
+    shader))
 
+(make-shader "line-shader"
+             "
+             #version 150 core
+		     in vec3 in_pos;
+		     uniform mat4 proj;
+		     uniform mat4 moview;
+		     void main() {
+		     	gl_Position = proj * moview * vec4(in_pos,1);
+		     }"
+             "
+             #version 150 core
+             out vec4 out_col;
+             uniform vec3 line_col;
+             void main() {
+             	out_col = vec4(line_col,1);
+             }"
+             (list "in_pos"))
 
 (make-perspective-camera "cam-name" (list 0 0 5) (list 0 0 1) (list 0 1 0) 45 1 0.1 1000)
 
-;/make_texture("mytex", "/home/kai/Desktop/a-gnu.png", GL_TEXTURE_2D);
-(texture-from-file "gnutex" "/home/kai/Desktop/a-gnu.png" 'tex2d)
+(texture-from-file "gnutex" "/home/kai/render-data/images/floor.png" 'tex2d)
