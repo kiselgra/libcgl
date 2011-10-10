acabe35311124b0382730b52faf572c58137250c
Kai <stuff@projects.selgrad.org>
2011-10-10 21:09:01 +0200

    file-finder and such.

--- src/impex.cpp
+++ src/impex.cpp
@@ -4,88 +4,89 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <list>
+#include <string>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+using namespace std;
 
 extern "C" {
 
-vec3f* load_png3f(const char *filename, unsigned int *w, unsigned int *h) {
-	png::image<png::rgb_pixel> image(filename);
-	*w = image.get_width(),
-	*h = image.get_height();
-	vec3f *pixels = (vec3f*)malloc(sizeof(vec3f)**w**h);
-	for (size_t y = 0; y < *h; ++y)
-		for (size_t x = 0; x < *w; ++x) {
-			png::rgb_pixel px = image.get_pixel(x, *h-y-1);
-			make_vec3f(pixels+y**w+x, px.red/255.0f, px.green/255.0f, px.blue/255.0f);
-		}
-	return pixels;
-}
-
-void save_png3f(const vec3f *data, unsigned int w, unsigned int h, const char *filename) {
-	png::image<png::rgb_pixel> image(w, h);
-	for (size_t y = 0; y < h; ++y)
-		for (size_t x = 0; x < w; ++x) {
-			const vec3f *v = data+y*w+x;
-			image[h-y-1][x] = png::rgb_pixel(v->x*255.0f, v->y*255.0f, v->z*255.0f);
-		}
-	image.write(filename);
-}
-
-void save_png4f(const vec4f *data, unsigned int w, unsigned int h, const char *filename) {
-	png::image<png::rgba_pixel> image(w, h);
-	for (size_t y = 0; y < h; ++y)
-		for (size_t x = 0; x < w; ++x) {
-			const vec4f *v = data+y*w+x;
-			image[h-y-1][x] = png::rgba_pixel(v->x*255.0f, v->y*255.0f, v->z*255.0f, v->w*255.0f);
-		}
-	image.write(filename);
-}
-
-}
-
-/*
-#include <wand/MagickWand.h>
-#include <wand/magick-property.h>
-
-int test_im(char *in, char *out)
-{
-#define ThrowWandException(wand) \
-	{ \
-		char \
-		*description; \
-		\
-		ExceptionType \
-		severity; \
-		\
-		description=MagickGetException(wand,&severity); \
-		(void) fprintf(stderr,"%s %s %lu %s\n",GetMagickModule(),description); \
-		description=(char *) MagickRelinquishMemory(description); \
-		exit(-1); \
+	vec3f* load_png3f(const char *filename, unsigned int *w, unsigned int *h) {
+		png::image<png::rgb_pixel> image(filename);
+		*w = image.get_width(),
+		*h = image.get_height();
+		vec3f *pixels = (vec3f*)malloc(sizeof(vec3f)**w**h);
+		for (size_t y = 0; y < *h; ++y)
+			for (size_t x = 0; x < *w; ++x) {
+				png::rgb_pixel px = image.get_pixel(x, *h-y-1);
+				make_vec3f(pixels+y**w+x, px.red/255.0f, px.green/255.0f, px.blue/255.0f);
+			}
+		return pixels;
 	}
 
-	MagickBooleanType
-		status;
+	void save_png3f(const vec3f *data, unsigned int w, unsigned int h, const char *filename) {
+		png::image<png::rgb_pixel> image(w, h);
+		for (size_t y = 0; y < h; ++y)
+			for (size_t x = 0; x < w; ++x) {
+				const vec3f *v = data+y*w+x;
+				image[h-y-1][x] = png::rgb_pixel(v->x*255.0f, v->y*255.0f, v->z*255.0f);
+			}
+		image.write(filename);
+	}
 
-	MagickWand
-		*magick_wand;
+	void save_png4f(const vec4f *data, unsigned int w, unsigned int h, const char *filename) {
+		png::image<png::rgba_pixel> image(w, h);
+		for (size_t y = 0; y < h; ++y)
+			for (size_t x = 0; x < w; ++x) {
+				const vec4f *v = data+y*w+x;
+				image[h-y-1][x] = png::rgba_pixel(v->x*255.0f, v->y*255.0f, v->z*255.0f, v->w*255.0f);
+			}
+		image.write(filename);
+	}
 
-	MagickWandGenesis();
-	magick_wand=NewMagickWand();
-	status=MagickReadImage(magick_wand,in);
-	if (status == MagickFalse)
-		ThrowWandException(magick_wand);
-	double x,y;
-	MagickGetResolution(magick_wand, &x, &y);
-	printf("X: %e Y: %e\n", x, y);
+	std::list<std::string> image_paths;
+	struct initial_paths
+	{
+		initial_paths()
+		{
+			image_paths.push_back(".");
+		}
+	} initialize_paths;
+	
+	bool file_exists(const char *name)
+	{
+		struct stat st;
+		int stat_res = stat(name, &st);
+		if (stat_res == 0)
+			return true;
+		else
+			return false;
+	}
 
-	MagickResetIterator(magick_wand);
-	while (MagickNextImage(magick_wand) != MagickFalse)
-		MagickResizeImage(magick_wand,106,80,LanczosFilter,1.0);
-	status=MagickWriteImages(magick_wand,out,MagickTrue);
-	if (status == MagickFalse)
-		ThrowWandException(magick_wand);
-	magick_wand=DestroyMagickWand(magick_wand);
-	MagickWandTerminus();
-	return(0);
+	char* find_file(const char *basename)
+	{
+		for (std::list<std::string>::const_iterator it = image_paths.begin(); it != image_paths.end(); ++it)
+		{
+			string p = *it;
+			if (p[p.length()-1] != '/')
+				p += "/";
+			p += basename;
+			if (file_exists(p.c_str()))
+				return strdup(p.c_str());
+		}
+		return 0;
+	}
+
+	void append_image_path(const char *path) {
+		image_paths.push_back(path);
+	}
+
+	void prepend_image_path(const char *path) {
+		image_paths.push_front(path);
+	}
 }
 
-*/
--- src/impex.h
+++ src/impex.h
@@ -13,6 +13,11 @@ extern "C" {
 vec3f* load_png3f(const char *filename, unsigned int *w, unsigned int *h);
 void save_png3f(const vec3f *data, unsigned int w, unsigned int h, const char *filename);
 void save_png4f(const vec4f *data, unsigned int w, unsigned int h, const char *filename);
+	
+void append_image_path(const char *path);
+void prepend_image_path(const char *path);
+bool file_exists(const char *name);
+char* find_file(const char *basename);
 
 #ifdef __cplusplus
 }
