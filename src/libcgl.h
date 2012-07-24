4a76ddb768208d66431603d931d7434c780ca9bd
kai <kai@istar>
2012-07-24 11:42:13 +0200

    libcgl header.

---
+++ src/libcgl.h
@@ -0,0 +1,16 @@
+#ifndef __LIBCGL_H__ 
+#define __LIBCGL_H__ 
+
+		#include "cgl.h"
+		#include "glut.h"
+		#include "shader.h"
+		#include "mesh.h"
+		#include "texture.h"
+		#include "camera.h"
+		#include "objloader.h"
+		#include "impex.h"
+		#include "scheme.h"
+		#include "framebuffer.h"
+
+#endif
+
