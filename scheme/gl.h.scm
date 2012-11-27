79fed9bb93ca60248fc7a8b6b8a37bff3e267633
kai <kai@deathmetal>
2012-11-27 13:56:09 +0100

    buildsystem fix.

--- scheme/gl.h.scm
+++ scheme/gl.h.scm
@@ -1,4 +1,4 @@
-;;; generated from the distributed gl3.h by cgl on Tue Nov 20 17:12:42 CET 2012.
+;;; generated from the distributed gl3.h by cgl on Tue Nov 27 13:25:27 CET 2012.
 (define gl#es-version-3-0                                1)
 (define gl#es-version-2-0                                1)
 (define gl#depth-buffer-bit                              #x00000100)
