764f808e6998daf1273fdf73a96f7091ba408f31
kai <kai@istar>
2012-12-17 00:23:02 +0100

    .

--- scheme/gl.h.scm
+++ scheme/gl.h.scm
@@ -1,4 +1,4 @@
-;;; generated from the distributed gl3.h by cgl on Sun Dec 16 19:39:52 CET 2012.
+;;; generated from the distributed gl3.h by cgl on Mon Dec 17 00:01:33 CET 2012.
 (define gl#es-version-3-0                                1)
 (define gl#es-version-2-0                                1)
 (define gl#depth-buffer-bit                              #x00000100)
