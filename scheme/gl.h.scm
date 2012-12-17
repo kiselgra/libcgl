71b77d44bae6504ab3491ffb5b7d7042cb5aed18
kai <kai@deathmetal>
2012-12-17 10:48:06 +0100

    stupid bug in configure.

--- scheme/gl.h.scm
+++ scheme/gl.h.scm
@@ -1,4 +1,4 @@
-;;; generated from the distributed gl3.h by cgl on Mon Dec 17 00:01:33 CET 2012.
+;;; generated from the distributed gl3.h by cgl on Mon Dec 17 10:43:39 CET 2012.
 (define gl#es-version-3-0                                1)
 (define gl#es-version-2-0                                1)
 (define gl#depth-buffer-bit                              #x00000100)
