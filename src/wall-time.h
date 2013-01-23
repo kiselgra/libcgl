25179f640321494266eb4316ca21499cc705fa2e
kai <kai@deathmetal>
2013-01-23 19:37:36 +0100

    c++

--- src/wall-time.h
+++ src/wall-time.h
@@ -1,9 +1,16 @@
 #ifndef __CGL_WALL_TIME_H__ 
 #define __CGL_WALL_TIME_H__ 
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 typedef double wall_time_t;
 wall_time_t wall_time_in_ms();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
 
